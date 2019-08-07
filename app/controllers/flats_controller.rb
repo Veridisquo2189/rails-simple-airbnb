4143c7ba154e37a14b81779a643077043c01fc7f
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-08-07 16:24:36 +0200

    add a flat done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,21 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Big flat in London',
+  address: 'Baker street',
+  description: 'The famous flat of Sherlock Holmes',
+  price_per_night: 95,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'small palace, nothing fancy',
+  address: 'Buckingham Palace',
+  description: 'You have an old lady as roomie',
+  price_per_night: 90_000_000,
+  number_of_guests: 90_001
+)
+Flat.create!(
+  name: 'Phone Cabin in London',
+  address: 'Nottingham',
+  description: 'The phone cabin of Doctor Who',
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
